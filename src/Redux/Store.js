f1773d34845ff817ff5a36c16037bb418ffa7df0
Ashish Thotwe <ashishthotwe20@gmail.com>
2024-01-29 14:07:28 +0530

    updated readme

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,31 +1,27 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
 import { conversationReducer } from "./Reducers/conversationSlice";
 
-// Configuration for redux-persist
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 const persistConfig = {
-  key: "root", // Key for the persisted state
-  storage, // Storage mechanism (e.g., localStorage)
+  key: "root",
+  storage,
   blacklist: ["register"], // Exclude the 'register' part from being persisted
 };
 
-// Wrap conversationReducer with persistReducer
 const persistedReducer = persistReducer(persistConfig, conversationReducer);
 
-// Configure the Redux store
 const store = configureStore({
   reducer: {
-    conversationReducer: persistedReducer, // Set up reducer with persisted state
+    conversationReducer: persistedReducer,
   },
-  middleware: getDefaultMiddleware({
-    // Disable serializable check to avoid warning/error with redux-persist
+  middleware: getDefaultMiddleware =>
+  getDefaultMiddleware({
     serializableCheck: false,
   }),
 });
 
-// Create persistor for persisting state
 export const persistor = persistStore(store);
-
-// Export the Redux store
 export default store;
